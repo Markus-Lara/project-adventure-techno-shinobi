9f9898cb5c170515dcd046485f25c7d0c555dacf
Markus-Lara <tcjl@student.cart.org>
2026-04-22 10:43:11 -0700

    Added Frostveil

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
-const intro = document.getElementById("start-expo")
+const intro = document.getElementById("startexpo")
 intro.addEventListener("click", function() {
-intro.innerHTML = "Hello";
+intro.innerHTML = "It's the year 2150, and like any other normal day in Japan, I am on my way to my personal augmentation business, Augmentation Revelation. Here in my business, I do my usual: watch over my employees, do personal augmentations for VIPs, and then I head home, let my robot chef cook me some dinner, go to bed and sleep, wake up, repeat.";
 intro.style.color = "red";
-localStorage.setItem(intro.true);
+localStorage.setItem(startexpo, true);
 });
+
+
