c60addf88978902ac8da6d7b359d5765dbf6c4f4
Randy <rjcowboys001@gmail.com>
2026-04-23 10:35:41 -0700

    Revising Java script, set module java script

---
+++ story.js
@@ -0,0 +1,17 @@
+const intro = document.getElementById("startexpo")
+intro.addEventListener("click", function() {
+intro.innerHTML = 'So today, I opened a new manager position to work with Benjamin, since he has to watch over so much already. I put up some signs around the city looking for a new manager position, and just as I had hoped, three candidates came into the business. There\'s one Jessie Pinkman, who was so nervous I could see her shaking so much, which wasn\'t a great quality for the job here.\n There was one Rowan Todd, who seemed great at the inteerview but looking at his previous applications they usually ended on a sour note, which isn\'t good either. The the last participant James Loplin, however, seemed perfect. He answered every question about the job perfectly, and his qualifications and previous work places spoke highly of him. I decided that he would be the perfect candidate.'
+});
+
+//const story = { //discontinued for now
+//intro {
+ //   'It\'s the year 2150, and like any other normal day in Japan, I am on my way to my personal augmentation business, Augmentation Revelation. Here in my business, I do my usual: watch over my employees, do personal augmentations for VIPs, and then I head home, let my robot chef cook me some dinner, go to bed and sleep, wake up, repeat.\nBut today is a different day than usual. I\'ve had this business for 5 years, and it\'s significant growth has pushed me to focus ideas such as expansion an other business ideas since I have gotten so many personal request over the past few months, and I just can\'t handle them all.'
+//}
+//};
+
+const WanobaC = {
+    branch: {
+    text: 'The three days goes by, and he\'s officially implemented in the job space. Everything was well at first: he\'s getting along with everyone, he is doing a good job managing not only employees but special requests as well, then I notice little inconsistencies. I notice how sometimes he calls off sick for days at a time, or when he thinks he\'s alone I hear him talking to himself. And during one of those sessions I hear the name Wanoba.\n It was the end of the day so I couldn\'t question, and when I got back home I did some research into the name. I couldn\'t find anything, so I had to pick up research tomorrow. Back at the business I\'m in my office, and the lights go out, so I go to investigate. Walking through the hallways, there was some whisperings and mumblings as I walked ot the back to fix the breaker. There was no light, so I had to stumble in the darkness for a second. I find he box, fix it, and head on my way back. I\'m about to leave, then I notice something in the corner of my eye. I look, and there is someone on the ground.\n "BEN!!" Before I can turn, I notice a dark figure shuffling in the dark, then they move in front of the breaker box. What am I gonna do?',
+choice: [["Run", "AA"], ["Hold", "AB"], ["Investigate", "AC"]]
+}
+};
