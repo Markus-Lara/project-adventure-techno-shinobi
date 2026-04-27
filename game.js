12cb9e87c21f4e2de370eee03072836b43ab3cb2
Markus-Lara <tcjl@student.cart.org>
2026-04-27 10:39:44 -0700

    Quick fix

--- game.js
+++ game.js
@@ -1,11 +1,10 @@
 /* Characteristics that will stay fixed for different classes of enemy characters */
-const EnemyTankCharacter = [{health: "150"}, {attack: "8"}]
-const EnemyAttackCharacter = [{health: "50"}, {attack: "12"}]
-const EnemyBalancedCharacter = [{health: "100"}, {attack: "10"}]
+const EnemyTankCharacter = [{health: 150}, {attack: 8}]
+const EnemyAttackCharacter = [{health: 50}, {attack: 12}]
+const EnemyBalancedCharacter = [{health: 100}, {attack: 10}]
 
-const AllCites = [{Boss: "1"}, {Items: "3"}, {MovesRecieved: "1"}]
-const Frostveil = [{Battles: "7"}, {Tank: "Polar Bearer"}, {Attacker: "Penguinja"}, {Balanced: "Sea Lion"}, {Ally: "Hans"}, {BossName: "Kamiron"}]
-const LambdaCentral = [{Battles: "8"}, {Tank: "M.A.B"}, {Attacker: "A.S.R"}, {Balanced: "A.H.G"}, {Ally: "Bridgette"}, {BossName: "Boomera"}]
-const GrumodaJungle = [{Battles: "7"}, {Tank: "Bomber Regy"}, {Attacker: "Attacker Floresta"}, {Balanced: "WeedWhip"}, {Ally: "Clerica"}, {BossName: "Boss Shieva and Brass Knuckles"}]
-var Jason = [{health: "100"}, {Damage: ""}]
+const Frostveil = [{Battles: 7}, {Tank: "Polar Bearer"}, {Attacker: "Penguinja"}, {Balanced: "Sea Lion"}, {Ally: "Hans"}, {BossName: "Kamiron"}]
+const LambdaCentral = [{Battles: 8}, {Tank: "M.A.B"}, {Attacker: "A.S.R"}, {Balanced: "A.H.G"}, {Ally: "Bridgette"}, {BossName: "Boomera"}]
+const GrumodaJungle = [{Battles: 7}, {Tank: "Bomber Regy"}, {Attacker: "Attacker Floresta"}, {Balanced: "WeedWhip"}, {Ally: "Clerica"}, {BossName: "Boss Shieva and Brass Knuckles"}]
+var Jason = [{health: 100}, {Damage: ""}]
 
